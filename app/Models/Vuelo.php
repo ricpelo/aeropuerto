692812f3f71f0801ef81744b3e57f9680cadfc4a
Ricardo Pérez <ricpelo@gmail.com>
2024-05-06 20:27:52 +0200

    Después de codificar los modelos y relaciones

---
+++ app/Models/Vuelo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vuelo extends Model
+{
+    use HasFactory;
+
+    public function compania()
+    {
+        return $this->belongsTo(Compania::class);
+    }
+
+    public function origen()
+    {
+        return $this->belongsTo(Aeropuerto::class, 'origen_id');
+    }
+
+    public function destino()
+    {
+        return $this->belongsTo(Aeropuerto::class, 'destino_id');
+    }
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class);
+    }
+}
---
+++ database/migrations/2024_05_06_180039_create_aeropuertos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aeropuertos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo', 3)->unique();
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE aeropuertos
+                        ADD CONSTRAINT ck_codigo_letras
+                        CHECK (codigo SIMILAR TO '%[A-Z]{3}%')");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aeropuertos');
+    }
+};
